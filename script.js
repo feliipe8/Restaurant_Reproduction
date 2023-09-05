be9b15b2f85ac39c6a773e68a63fc21a53e6533d
Felipe Estrada <87234125+feliipe8@users.noreply.github.com>
2023-09-04 22:13:11 -0500

    Commit1
    This is the first reproduction of the web example.

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var menuList = document.getElementById('menu-list');
+    var menuItems = [
+      { name: 'Plato 1  ', description: 'Menú ejecutivo', price: '$10.000 pesos' },
+      { name: 'Plato 2  ', description: 'Cazuela de frijoles', price: '$15.000 pesos' },
+      { name: 'Plato 3  ', description: 'Consume de pescado', price: '$12.000 pesos' }
+    ];
+  
+    menuItems.forEach(function(item) {
+      var li = document.createElement('li');
+      var name = document.createElement('span');
+      name.textContent = item.name;
+      var description = document.createElement('span');
+      description.textContent = item.description;
+      var price = document.createElement('span');
+      price.textContent = item.price;
+      li.appendChild(name);
+      li.appendChild(description);
+      li.appendChild(price);
+      menuList.appendChild(li);
+    });
+  
+    
+    var contactForm = document.getElementById('contact-form');
+    contactForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      var nameInput = document.getElementById('name');
+      var emailInput = document.getElementById('email');
+      var messageInput = document.getElementById('message');
+  
+      
+      console.log('Nombre:', nameInput.value);
+      console.log('Email:', emailInput.value);
+      console.log('Mensaje:', messageInput.value);
+  
+     
+      nameInput.value = '';
+      emailInput.value = '';
+      messageInput.value = '';
+    });
+  });
+  
